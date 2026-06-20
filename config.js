2a74cdb23866e37a89e2c6689ce78076ca07ef3e
Pratham Jain <prathamjain@Prathams-MacBook-Air.local>
2026-06-19 23:02:31 -0700

    Add GPA fact, raise message limit to 20

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
   WAITLIST_FORM:  "https://formspree.io/f/mjgzeyld",
   FEEDBACK_FORM:  "https://formspree.io/f/xaqkdevr",
-  MESSAGE_LIMIT:  10,
+  MESSAGE_LIMIT:  20,
   MODEL:          "gemini-2.5-flash-lite",
 };
